bf714c13278cd7de3428da5c641074f48d708be7
JosueUPT <jaechambilla@upt.pe>
2024-07-01 17:26:08 -0500

    dockerizacion en proceso

--- Interfaz/app/config.py
+++ Interfaz/app/config.py
@@ -1,9 +1,9 @@
 class DevelopmentConfig():
     DEBUG = True
-    MYSQL_HOST = 'localhost'
+    MYSQL_HOST = '159.89.107.90'
     MYSQL_PORT = 3306
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'chatbot'
+    MYSQL_PASSWORD = '123456'
     MYSQL_DB = 'chatbot'
 
 config = {
